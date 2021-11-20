cf41523b5c9613cfbd036b7b4c8978900d694e22
Shakeel Khan <shakeelk02@hotmail.com>
2021-11-19 20:12:56 -0800

    Got aspect ratio and black bars working for Menu scene and removed delay in the beginning of menu music

---
+++ Assets/Scripts/Camera/MaintainAspectRatio.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaintainAspectRatio : MonoBehaviour
+{
+    public float TARGET_ASPECT_RATIO = 2.0f / 3.0f;
+
+    // Use this for initialization
+    void Start()
+    {
+        // Determine the game window's current aspect ratio.
+        float windowAspectRatio = (float)Screen.width / (float)Screen.height;
+
+        // Current viewport height should be scaled by this amount.
+        float scaleHeight = windowAspectRatio / TARGET_ASPECT_RATIO;
+
+        // Obtain camera component so we can modify its viewport.
+        Camera camera = GetComponent<Camera>();
+
+        // If scaled height is less than current height, add letterbox.
+        if (scaleHeight < 1.0f)
+        {
+            Rect rect = camera.rect;
+
+            rect.width = 1.0f;
+            rect.height = scaleHeight;
+            rect.x = 0;
+            rect.y = (1.0f - scaleHeight) / 2.0f;
+
+            camera.rect = rect;
+        }
+        else
+        {
+            // Add pillarbox.
+            float scaleWidth = 1.0f / scaleHeight;
+
+            Rect rect = camera.rect;
+
+            rect.width = scaleWidth;
+            rect.height = 1.0f;
+            rect.x = (1.0f - scaleWidth) / 2.0f;
+            rect.y = 0;
+
+            camera.rect = rect;
+        }
+    }
+}
