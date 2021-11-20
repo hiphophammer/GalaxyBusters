efde7e86114eb93651437a319df8fd60f2a89e87
Shakeel Khan <shakeelk02@hotmail.com>
2021-11-19 21:09:58 -0800

    fixed player bounds checking

--- Assets/Scripts/Player/MovementBehaviors/BaseMovement.cs
+++ Assets/Scripts/Player/MovementBehaviors/BaseMovement.cs
@@ -11,6 +11,9 @@ public class BaseMovement : MonoBehaviour
     private string[] movementAxes;
     private float speed;
 
+    private float maxXPos = (20.0f / 3.0f) / 2.0f;
+    private float maxYPos = 5.0f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,6 +22,9 @@ public class BaseMovement : MonoBehaviour
 
         movementAxes = parent.GetMovementAxes();
         speed = parent.GetSpeed();
+
+        maxXPos -= (parent.transform.localScale.x / 2.0f);
+        maxYPos -= (parent.transform.localScale.y / 2.0f);
     }
 
     // Update is called once per frame
@@ -33,26 +39,24 @@ public class BaseMovement : MonoBehaviour
         // Scale the values appropriately and check for bounds
         Vector3 pos = transform.position;
         upDown *= (speed * Time.deltaTime);
-        if(upDown < 0 && pos.y <= -5){
-            upDown = 0;
-        } else if(upDown > 0 && pos.y >= 5){
-            upDown = 0;
+        if ((upDown < 0.0f) && (pos.y <= (-1.0f * maxYPos + 0.4f)))
+        {
+            upDown = 0.0f;
+        }
+        else if ((upDown > 0.0f) && (pos.y >= maxYPos))
+        {
+            upDown = 0.0f;
         }
 
         leftRight *= (speed * Time.deltaTime);
-        if(leftRight < 0 && pos.x <= -3.335962){
-            leftRight = 0;
-        } else if(leftRight > 0 && pos.x >= 3.335962){
-            leftRight = 0;
+        if ((leftRight < 0.0f) && (pos.x <= (-1.0f * maxXPos)))
+        {
+            leftRight = 0.0f;
         }
-
-        /* This broke, not sure why, added bandage fix above
-        ///////////////////////////////////////////////////
-        // Get the bounds of our renderer.
-        Bounds myBound = GetComponent<Renderer>().bounds;
-
-        // Check if our bounds exceed that of the camera.
-        CameraSupport.WorldBoundStatus status = cameraSupport.CollideWorldBound(myBound);*/
+        else if ((leftRight > 0.0f) && (pos.x >= maxXPos))
+        {
+            leftRight = 0.0f;
+        }
 
         // Translate the player by the computed amount.
         transform.Translate(leftRight, upDown, 0.0f);
