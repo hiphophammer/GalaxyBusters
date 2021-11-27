a7099a5e36fc1360047d69a8095ca71c2752dfbb
Gary Yuen <gy1696@gmail.com>
2021-11-26 22:13:33 -0800

    Changed movement behavior for Trailblazer; Ultimate should now charge when enemy destroyed.
    - Camera Bounds tweaked a little bit; needs testing.
    - All existing enemies have had a reference to PlayerBehavior's DestroyedEnemy function added to charge ultimate.
    - Added a reset position to GameManager so that player doesn't have to fly all the way back.

--- Assets/Scripts/Enemy/ChaserBehavior.cs
+++ Assets/Scripts/Enemy/ChaserBehavior.cs
@@ -105,6 +105,9 @@ public class ChaserBehavior : MonoBehaviour, EnemyBehavior
         {
             // As a HeroProjectile, other must have a ProjectileBehavior script attached.
             ProjectileBehavior damageDealer = other.GetComponent<ProjectileBehavior>();
+            PlayerBehavior p = damageDealer.GetParent();
+            p.DestroyedEnemy();
+            Debug.Log(damageDealer.GetParent());
             health.decreaseHealth(damageDealer.GetParent());
         }
     }
